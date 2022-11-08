1f11dbf075794f7900544d19bf559f176f4821f4
Lorin de Lepper <lorindelepper@gmail.com>
2022-11-08 11:56:37 +0100

    initial commit

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_that_true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_that_name_is_jack()
+    {
+        $name = "Jack";
+        // $name = "Jack";
+        $this->assertTrue($name == "Jack");
+    }
+}
